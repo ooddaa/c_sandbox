9481cf3b334aa03f9a530e7dc48a4d6883f919cb
oda <ooddaa@gmail.com>
2024-04-10 20:59:28 +0100

    [c_book] 1-ch/ex-1-13 words' lengths horizontal histogram

---
+++ c_book/1-ch/exercises/ex-1-13.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+
+#define IN  1
+#define OUT 0
+#define MAX_BARS 20
+
+/*
+ * Write program that prints the histogram
+ * of the lenghts of the words in its input.
+ * 
+ * DONE:
+ * - print total for each histogram in the 
+ *   end of the line:  
+ *   1   |              [1]
+ *   2   ||||           [4]
+ *
+ * - print header: 
+ *   Len Count          Total
+ *   1   |              [1]
+ *   2   ||||           [4]
+ *
+ * TODO: 
+ * - make it print vertical bars
+ */
+
+int main(void) 
+{
+  int i, c, bars, wl, state, is_letter; 
+  int words[11]; // [1_letter_words, 2_letter_words...10+_letter_words]
+
+  wl = 0; // word length 
+  state = OUT; 
+
+  for (i = 0; i < 11; ++i) words[i] = 0;
+  
+
+  while ((c = getchar()) != EOF) {
+    is_letter = (c != ' ' && c != '\n' && c != '\t');
+    
+    if (state == OUT && is_letter) {
+      state = IN;
+      ++wl; 
+    } else if (state == IN && is_letter) {
+      ++wl;
+    } else if (state == IN && !is_letter) {
+      state = OUT;
+      ++words[wl-1]; // who doesn't love offets
+      wl = 0;
+    }
+  }
+
+  printf("Len Count                   Total\n");
+  for (i = 0; i <= 10; ++i) {
+    if (i == 9) {
+      printf("10  ");
+    } else if (i == 10) {
+      printf("10+ ");
+    } else {
+      printf("%d", i + 1);
+      printf("   ");
+    }
+
+    // print bars, limit 
+    for (bars = 1; bars <= words[i] ; ++bars) {
+      if (bars < MAX_BARS) printf("|");
+      else if (bars == MAX_BARS) printf("|+");
+    }
+    
+    // calculate left offset to print "[total_count]"
+    // [max_bars][+][1ws]
+    // MAX_BARS + 1 + 1
+    for (int j = 0; j < (MAX_BARS + 2) - bars; ++j) {
+      printf(" ");
+    }
+    printf("   [%d]\n", words[i]);
+  } 
+
+  return 0;
+}
