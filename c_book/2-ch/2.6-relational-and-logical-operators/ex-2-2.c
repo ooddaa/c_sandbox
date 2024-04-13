201256a0f41309a23a536bea535d96b6ea3b769a
oda <ooddaa@gmail.com>
2024-04-13 13:54:23 +0100

    [c_book] fix bug in ex-2-2

--- c_book/2-ch/2.6-relational-and-logical-operators/ex-2-2.c
+++ c_book/2-ch/2.6-relational-and-logical-operators/ex-2-2.c
@@ -5,8 +5,12 @@ int main(void)
   int i, lim = 10;
   char c;
   char s[lim];
-  for (i=0, c=getchar(); (i<lim-1) + (c != '\n') + (c != EOF) == 0; ++i) {
+  
+  // Don't forget to update c ! Otherwise we always end up with the first character. 
+  // If we put (c = getchar()) into condition section, we'll get "unsequenced access".
+  for (i = 0, c = getchar(); i < lim-1 && (c != '\n') && (c != EOF); ++i, c = getchar()) {
     s[i] = c;
   }
+  printf("%s\n", s);
   return 0;
 }
