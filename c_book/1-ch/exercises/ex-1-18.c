60a4003ad25328f6753cf69f9c8336b901aec922
oda <ooddaa@gmail.com>
2024-04-14 12:24:48 +0100

    [c_book] ex-1-18 tidy up

--- c_book/1-ch/exercises/ex-1-18.c
+++ c_book/1-ch/exercises/ex-1-18.c
@@ -8,10 +8,6 @@
  * Prints results to stdout. 
  * */
 
-// get new line 
-// get a line, save it 
-// walk it backwards, deleting ' ' and '\t'
-// until c == isletter 
 int getnextline(char[], int);
 void trimline(char[], int); 
 
@@ -38,19 +34,13 @@ int getnextline(char s[], int limit)
 void trimline(char s[], int len) 
 {
    int i = len - 1;
-   // printf("len: %d\n", i); 
-   // while (i > 0 && (s[i] != ' ' || s[i] != '\t') && s[i] != 's') { 
    while (i > 0 && (s[i] == ' ' || s[i] == '\t') || s[i] == '\n' || s[i] == '\0') { 
     --i;
-    // printf("s[%d]: %c", i, s[i]);
-   // printf("--len: %d\n", i); 
    }
-  // printf("s[%d] before: %c", i, s[i]);
    if (i != 0) {
      ++i;
      s[i] = '\n';
      ++i;
-    // printf("s[%d] after: %c", i, s[i]);
      s[i] = '\0';
   } else {
     s[0] = '\0';
@@ -60,12 +50,11 @@ void trimline(char s[], int len)
 
 int main(void)
 {
-  int len, count = 1;
+  int len;
   char s[MAX];
-  // while we have a line 
+
   while (getnextline(s, MAX) != 0) {
     printf("%s", s);
-    --count;
   }
 
 
