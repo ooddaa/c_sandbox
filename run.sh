732f435c6bea0051c04befdfedaacd0fa46afe73
oda <ooddaa@gmail.com>
2024-05-12 22:04:45 +0100

    renamed files for number consistency

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 function compile_and_run_c_program() {
   local FILENAME=$1
   local PATH_TO_BIN=$(echo $FILENAME | awk 'gsub("/", "-") gsub("\.c$", "") {print "./bin/"$1}')
-  clang -o $PATH_TO_BIN  $FILENAME && chmod +x $PATH_TO_BIN && $PATH_TO_BIN   
+  clang -o $PATH_TO_BIN $FILENAME && chmod +x $PATH_TO_BIN && $PATH_TO_BIN   
 }
 
 compile_and_run_c_program $1 
