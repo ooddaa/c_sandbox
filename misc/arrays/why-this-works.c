cfb7b0fcd7ebdce0ed06383d6e282e489710cbab
oda <ooddaa@gmail.com>
2024-05-14 18:22:25 +0100

    i[arr] thing and c version

---
+++ misc/arrays/why-this-works.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+// https://www.youtube.com/watch?v=lubj1yUN9hw
+
+int main(void)
+{
+  int arr[5] = { 1, 2, 3, 4, 5 };
+
+  for (int i = 0; i < 5; ++i) 
+  {
+    printf("%d\n", i[arr]);
+  }
+
+  return 0;
+}
